e2fe519018ff41b04cbcc51cfa25cb83f8d27c36
Alireza Andersen <alireza.andersen@gmail.com>
2015-12-12 14:57:59 -0700

    update files

--- cryptkeeper.rb
+++ cryptkeeper.rb
@@ -30,7 +30,7 @@ class CryptKeeper
   end
 
 
-    def date_generator
+    def date_squared
     @date_key = Time.now
     @date_key.strftime("%d%m%y").to_i
     date_generator = date ** 2
@@ -40,6 +40,7 @@ class CryptKeeper
   end
 
   def date_rotation #creates custom digit for ABCD
+    date_generator
     @a_date_gen = @date_key[-4].to_i
     puts "@a_date_gen" + @a_date_gen.to_s
     @b_date_gen = @date_key[-3].to_i
@@ -50,30 +51,18 @@ class CryptKeeper
     puts "@d_date_gen" + @d_date_gen.to_s
   end
 
-  # def off_set_rotation
-  #   @a_date_key + @a_key = @a_rotation_code.to_i
-  #   puts "@a_date_key" + "@a_key" + @a_date_key + @a_key.to_s
-  #   @b_date_key + @b_key = @b_rotation_code.to_i
-  #   puts "@b_date_key" + "@b_key" + @b_date_key + @b_key.to_s
-  #   @c_date_key + @c_key = @c_rotation_code.to_i
-  #   puts "@c_date_key" + "@c_key" + @c_date_key + @c_key.to_s
-  #   @d_date_key + @d_key = @d_rotation_code.to_i
-  #   puts "@d_date_key" + "@d_key" + @d_date_key + @d_key.to_s
-  # end
+
 
   def off_set_rotation #combines key and date to give rotation
+    date_squared
     @a_rotation_code = @a_date_gen + @a_key_rotation
     puts "@a_rotation_code" + @a_date_gen.to_s + @a_key_rotation.to_s
-  #  puts "@a_date_gen" + "@a_key_rotation" + @a_date_gen + @a_key.to_s
     @b_rotation_code = @b_date_gen + @b_key_rotation
     puts "@b_rotation_code" + @b_date_gen.to_s + @b_key_rotation.to_s
-  #  puts "@b_date_gen" + "@b_key_rotation" + @b_date_key + @b_key.to_s
     @c_rotation_code = @c_date_gen + @c_key_rotation
     puts "@c_rotation_code" + @c_date_gen.to_s + @c_key_rotation.to_s
-  #  puts "@c_date_gne" + "@c_key_rotation" + @c_date_key + @c_key.to_s
     @d_rotation_code = @d_date_gen + @d_key_rotation
     puts "@d_rotation_code" + @d_date_gen.to_s + @d_key_rotation.to_s
-  #  puts "@d_date_gen" + "@d_key_rotation" + @d_date_key + @d_key.to_s
   end
 
 end
@@ -81,5 +70,3 @@ end
 ck = CryptKeeper.new('121215','23451')
 ck.key_rotation('23451')
 ck.off_set_rotation
-
-#need to figure out why rotation isn't working
