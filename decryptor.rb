66defdf838d24ceda0a5a016244f7d9657565eab
Alireza Andersen <alireza.andersen@gmail.com>
2015-12-12 22:30:14 -0700

    update files

--- decryptor.rb
+++ decryptor.rb
@@ -1,4 +1,5 @@
 require_relative 'enigma'
+require_relative ' encryptor'
 
 class Dycryptor
 
--- enigma.rb
+++ enigma.rb
@@ -19,7 +19,7 @@ class Enigma
         encrypt_key = key
       end
       if date.nil?
-        date_key = @date_key.strftime("%d%m%y")
+        date_key = Time.now.strftime("%d%m%y")
       else
         date_key = date
     end
@@ -28,61 +28,12 @@ class Enigma
       end
 
     def decrypt(secert_message, key, date)
-      decryptor = deycryptor.new(secert_message, key, date)
+      decryptor = Deycryptor.new(secert_message, key, date)
       decryptor.decrypt
     end
 
     def create_new_key
-      @key = "%05d" % Random.new.rand(99999)
-    end
-
-    def key_rotation(key = nil) #creates 2 digit custom code for ABCD
-      if key == nil
-        @a_key_rotation = @key[0..1].to_i
-        puts "@a_key_rotation" + @a_key_rotation.to_s
-        @b_key_rotation = @key[1..2].to_i
-        puts "@b_key_rotation" + @b_key_rotation.to_s
-        @c_key_rotation = @key[2..3].to_i
-        puts "@c_key_rotation" + @c_key_rotation.to_s
-        @d_key_rotation = @key[3..4].to_i
-        puts "@d_key_rotation" + @d_key_rotation.to_s
-      else
-        "%05d"
-      end
-    end
-
-    def date_generator
-      date = @date_key.strftime("%d%m%y").to_i
-      date_squared = date ** 2
-      puts "date_squared = " + date_squared.to_s
-      @off_sets = date_squared.to_s.split("")[-4..-1].join
-      puts "@off_sets = " + @off_sets.to_s
-    end
-
-    def date_rotation #creates custom digit for ABCD
-      date_generator
-      @a_date_gen = @off_sets[-4].to_i
-      puts "@a_date_gen" + @a_date_gen.to_s
-      @b_date_gen = @off_sets[-3].to_i
-      puts "@b_date_gen" + @b_date_gen.to_s
-      @c_date_gen = @off_sets[-2].to_i
-      puts "@c_date_gen" + @c_date_gen.to_s
-      @d_date_gen = @off_sets[-1].to_i
-      puts "@d_date_gen" + @d_date_gen.to_s
-      return [a,b,c,d]
-    end
-
-    def off_set_rotation #combines key and date to give rotation
-      date_rotation
-      date_generator
-      @a_rotation_code = @a_date_gen.to_i + @a_key_rotation.to_i
-      puts "@a_rotation_code" + @a_rotation_code.to_s
-      @b_rotation_code = @b_date_gen.to_i + @b_key_rotation.to_i
-      puts "@b_rotation_code" + @b_rotation_code.to_s
-      @c_rotation_code = @c_date_gen.to_i + @c_key_rotation.to_i
-      puts "@c_rotation_code" + @c_rotation_code.to_s
-      @d_rotation_code = @d_date_gen.to_i + @d_key_rotation.to_i
-      puts "@d_rotation_code" + @d_rotation_code.to_s
+       "%05d" % Random.new.rand(99999)
     end
 
 end
